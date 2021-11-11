6a80e9993afa4cf310f1987f76e3d8aebfbc1cca
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-11-11 16:19:45 -0500

    added sql file'

---
+++  nereus.sql
@@ -0,0 +1,11 @@
+USE nereus;  -- Select the database.
+
+-- insert data into the database
+
+INSERT INTO accountInfo VALUES
+(1,'sabamerchant', 'password@', 'svm21714@uga.edu'),
+(2,'venusyand', 'pass@w0rd', 'yang@gmail.com'),
+(3, 'sudhan', 'passw#d', 'sudhan@uga.edu'),
+(4, 'elodie', 'Yamaha', 'elodie@uga.edu');
+
+
