78c15402d2ad55943ab6fb43c7933fe18370b334
sudhan.chitgopkar <sudhanc03@yahoo.com>
2021-11-11 16:50:03 -0500

    added: mysqli connection functionality

---
+++ connDB.php
@@ -0,0 +1,11 @@
+    <?php
+    //==================== CONNECTING TO DB ====================//
+        $servername = "localhost"; 
+        $username = "root"; //default username
+        $password = "";     //default pass
+	$dbname = "nereus";
+
+        // Create connection
+        $mysqli = new mysqli($servername, $username, $password, $dbname);
+
+    ?> 
