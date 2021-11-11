ba859cac94dd02310aeb20b1ab1794a87728d19c
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-11-11 16:30:18 -0500

    added sql

---  nereus.sql
+++  nereus.sql
@@ -1,7 +1,8 @@
 USE nereus;  -- Select the database.
 
--- insert data into the database
+CREATE TABLE `nereus`.`accountInfo` ( `accountID` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(15) NOT NULL , `password` VARCHAR(15) NOT NULL , `email` VARCHAR(320) NOT NULL , PRIMARY KEY (`accountID`)) ENGINE = InnoDB;
 
+-- insert data into the database
 INSERT INTO accountInfo VALUES
 (1,'sabamerchant', 'password@', 'svm21714@uga.edu'),
 (2,'venusyand', 'pass@w0rd', 'yang@gmail.com'),
