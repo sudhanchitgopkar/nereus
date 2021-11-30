594ac603bea7e15e403e659ba03a8d810ccc91d7
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-11-30 17:09:38 -0500

    maybe format

--- browse.php
+++ browse.php
@@ -8,24 +8,26 @@
         <title>Browse</title>
     </head>
     <body>
-        <div>
+        <div style="text-align:center;">
             <span class="N-logo">N.</span>
             <br>
             <br>
             <br>
             <br>
-            <h1 style="text-align: center;">Your #1 Match:</h1>
-
-            <div class="frontBox" style="display: inline-block;height: 250px;left:35%">
-                <div style="display: inline-block;" class="circle">
-                    <div style="padding-left: 150px; font-size: 20px; padding-top: 20px;">Sudhan Chitgopkar</div>
-                </div>
-                <div style="font-size: 15px; text-align:center"> 
-                    <p>Hi! This is my bio. I might say some cheesy shit here, or tell a half-assed joke.</p>
-                    <p><b>Professional Memer</b></p>
-                    <p>Instagram • Twitter • Facebook</p>
+            <div >
+                <h1 style="text-align: center;">Your #1 Match:</h1>
+                <div class="frontBox" style="display: inline-block;height: 250px;">
+                    <div style="display: inline-block;" class="circle">
+                        <div style="padding-left: 150px; font-size: 20px; padding-top: 20px;">Sudhan Chitgopkar</div>
+                    </div>
+                    <div style="font-size: 15px; text-align:center"> 
+                        <p>Hi! This is my bio. I might say some cheesy shit here, or tell a half-assed joke.</p>
+                        <p><b>Professional Memer</b></p>
+                        <p>Instagram • Twitter • Facebook</p>
+                    </div>
                 </div>
             </div>
+            
 
             <?php
                 require('connDB.php');
