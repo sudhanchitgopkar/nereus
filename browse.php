31ca476e5499b11457d00cc9589d4a4b07abec13
Venusly Yang <venuslyyang@s172-20-116-h77.paws.uga.edu>
2021-11-30 01:21:26 -0500

    db

--- browse.php
+++ browse.php
@@ -1,48 +1,52 @@
 
-<!DOCTYPE html>
-<html>
-    <h1>pls work</h1>
-    <?php
-        require('connDB.php');
+    <!DOCTYPE html>
+    <html>
+        <h1>pls work</h1>
+        <?php
+            require('connDB.php');
+            
+            $sql = "SELECT username FROM accountInfo";
+            // $db = $conn;
+            // $result = mysqli_query($db, $sql);
+            $result = $conn->query($sql);
+        // $rows = $result -> fetchAll();
         
-        $sql = "SELECT username FROM accountInfo";
-        // $db = $conn;
-        // $result = mysqli_query($db, $sql);
-        $result = $conn->query($sql);
-        // $result = mysqli_query($conn, $sql);
-        // echo sizeof($result);
-        // echo [$result] . "<br>";
-        // var_dump($result);
-        // foreach($result as $row) {
-        //     echo $row . "<br>";
-        // }
-        $check_user = $result->num_rows > 0;
-        if($result->num_rows == 0) {
-            echo "no results";
-        }
-        else {
-            echo "results";
-        }
-        // $result = $conn->query($sql);
-        // echo $result;
-        if($check_user) {
-            echo 'inside if';
-        } else {
-            echo 'inside else';
-        }
-        echo 'after;';
+            // $result = mysqli_query($conn, $sql);
+            // echo sizeof($result);
+            // echo [$result] . "<br>";
+            // var_dump($result);
+            foreach($result as $row) {
+                echo $row['username'] . "<br>";
+            }
+            $check_user = $result->num_rows > 0;
+            if($result->num_rows == 0) {
+                echo "no results";
+            }
+            else {
+                echo "results";
+            }
+            // $result = $conn->query($sql);
+            // echo $result;
+
+            
+            if($check_user) {
+                echo 'inside if';
+            } else {
+                echo 'inside else';
+            }
+            echo 'after;';
         ?>
-    
+        
 
 
-    <h1>pls work before foreach</h1>
-    <?php foreach ($query as $name) : 
-        echo 'after for each'; ?>
-        <li>
-            <a class="linkColor" href="?category_id=<?php echo $name['username']; ?>">
-                <?php echo $name['username']; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
+        <h1>pls work before foreach</h1>
+        <?php foreach ($query as $name) : 
+            echo 'after for each'; ?>
+            <li>
+                <a class="linkColor" href="?category_id=<?php echo $name['username']; ?>">
+                    <?php echo $name['username']; ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
 
-</html>
+    </html>
