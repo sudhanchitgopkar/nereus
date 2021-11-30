013d96406248d5f69e1c5c1855332e0ff65babf0
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-11-30 00:31:49 -0500

    wtf

--- browse.php
+++ browse.php
@@ -1,105 +1,48 @@
-<?php
-
-include_once('session.php');
-
-
-// include('browse.php');
-?>
 
 <!DOCTYPE html>
-<html >
-    <head>
-        <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
-        <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
-        <link rel="stylesheet" href="browseCSS.css">
-        <title>Browse</title>
-    </head>
-    <body class="font">
-        <div style="display: inline-block;">
-            <h1 style="display: inline;" class="color">N.</h1>
-            <div class="topBar">
-                <div class="hamburger"></div>
-                <div class="hamburger"></div>
-                <div class="hamburger"></div>
-            </div>
-
-        </div> 
-        <h1 style="text-align: center;">Your #1 Match:</h1>
- 
-        <div class="frontBox" style="display: inline-block;">
-            <div style="display: inline-block;" class="circle">
-                <div style="padding-left: 150px; font-size: 25px; padding-top: 20px;">Sudhan Chitgopkar</div>
-            </div>
-            <div class="text">
-                <p>Hi! This is my bio. I might say some cheesy shit here, or tell a half-assed joke.</p>
-                <p><b>Professional Memer</b></p>
-                <p>Instagram • Twitter • Facebook</p>
-            </div>
-        </div>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <div>
-            <h2>Browse:</h2>
-            <!-- <div class="otherBox" style="display: inline-block;">
-                <div style="display: inline-block;" class="circle">
-                    <div style="padding-left: 150px; font-size: 25px; padding-top: 20px;">Sudhan Chitgopkar</div>
-                </div>
-                <div class="text">
-                    <p>Hi! This is my bio. I might say some cheesy shit here, or tell a half-assed joke.</p>
-                    <p><b>Professional Memer</b></p>
-                    <p>Instagram • Twitter • Facebook</p>
-                </div>
-            </div> -->
-
-            <!-- FROM THE VIDEO -->
-            <div class="container py-5">
-                <div class="row mt-4">
+<html>
+    <h1>pls work</h1>
+    <?php
+        require('connDB.php');
+        
+        $sql = "SELECT username FROM accountInfo";
+        // $db = $conn;
+        // $result = mysqli_query($db, $sql);
+        $result = $conn->query($sql);
+        // $result = mysqli_query($conn, $sql);
+        // echo sizeof($result);
+        // echo [$result] . "<br>";
+        // var_dump($result);
+        // foreach($result as $row) {
+        //     echo $row . "<br>";
+        // }
+        $check_user = $result->num_rows > 0;
+        if($result->num_rows == 0) {
+            echo "no results";
+        }
+        else {
+            echo "results";
+        }
+        // $result = $conn->query($sql);
+        // echo $result;
+        if($check_user) {
+            echo 'inside if';
+        } else {
+            echo 'inside else';
+        }
+        echo 'after;';
+        ?>
+    
 
 
-                    <?php
-                    //require database connection
-                    require ('db_connect.php');
-                    $query = "SELECT * FROM accountInfo";
-                    $query_run = mysqli_query($conn, $query);
+    <h1>pls work before foreach</h1>
+    <?php foreach ($query as $name) : 
+        echo 'after for each'; ?>
+        <li>
+            <a class="linkColor" href="?category_id=<?php echo $name['username']; ?>">
+                <?php echo $name['username']; ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
 
-                    $check_user = mysqli_num_rows() > 0;
-
-                    if($check_user) {
-                        while($row = mysqli_fetch_assoc($query_run)) { //could be array
-                            ?>
-                            <div class="col-md-4 mt-3">
-                                <div class="card">
-                                    <img src=" <?php echo $row['image'] ?> " width="200px" height="200px" alt="Profile Pic">  <!-- type in path into src -->   
-                                    <div class="card-body">
-                                        <h2 class="card-title"> <?php echo $row['username']; ?> </h2> <!--username would be name first and last -->
-                                        <p class="card-text"> <?php echo $row['email']; ?>  </p> <!--bio would be name first and last -->
-                                    </div>
-                                </div>
-                            </div>
-                            <?php 
-
-                            echo $row['username'];
-                        }
-                    } else {
-                        echo "No users found";
-                    }
-                    ?>
-                    
-                
-
+</html>
--- connDB.php
+++ connDB.php
@@ -1,21 +1,24 @@
 <?php
     //==================== CONNECTING TO DB ====================//
-        $db = parse_ini_file("config.ini");
+        // $db = parse_ini_file("config.ini");
 
-        $servername = $db['dbhost']; 
-        $username = $db['dbuser'];  //default username
-        $password = $db['dbpass'];      //default pass
-        $dbname = $db['dbname']; 
-
+        // $servername = $db['dbhost']; 
+        // $username = $db['dbuser'];  //default username
+        // $password = $db['dbpass'];      //default pass
+        // $dbname = $db['dbname']; 
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $db = "nereus";
         // Create connection
         try {
             $conn = new mysqli($servername, $username, $password, $dbname);
-            //  echo 'Connected successfully<br>';
+             echo 'Connected successfully<br>';
           }
           catch (PDOException $e)
           {
             $error=$e->getMessage();
-            // echo '<p> Unable to connect to database: ' .$error;
+            echo '<p> Unable to connect to database: ' .$error;
             exit();
           }
     ?> 
