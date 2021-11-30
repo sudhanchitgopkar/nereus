59b07f05b291591e120c107c177b7eeec8729f88
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-11-30 15:28:25 -0500

    formatting

--- browse.php
+++ browse.php
@@ -15,11 +15,11 @@
         <br>
         <h1 style="text-align: center;">Your #1 Match:</h1>
 
-        <div class="frontBox" style="display: inline-block;">
+        <div class="frontBox" style="display: inline-block;height: 250px;left:35%">
             <div style="display: inline-block;" class="circle">
                 <div style="padding-left: 150px; font-size: 20px; padding-top: 20px;">Sudhan Chitgopkar</div>
             </div>
-            <div class="text">
+            <div style="font-size: 15px; text-align:center"> 
                 <p>Hi! This is my bio. I might say some cheesy shit here, or tell a half-assed joke.</p>
                 <p><b>Professional Memer</b></p>
                 <p>Instagram • Twitter • Facebook</p>
@@ -32,25 +32,45 @@
             $sql = "SELECT username FROM accountInfo";
         
             $result = $conn->query($sql);
-        
-            // foreach($result as $row) {
-            //     echo $row['username'] . "<br>";
-            // }
-            // $check_user = $result->num_rows > 0;
-            // if($result->num_rows == 0) {
-            //     echo "no results";
-            // }
-            // else {
-            //     echo "results";
-            // }
-        
-            // if($check_user) {
-            //     echo 'inside if';
-            // } else {
-            //     echo 'inside else';
-            // }
-            // echo 'after;';
+            foreach($result as $row) {
+                echo $row['username'] . "<br>";
+            } 
         ?>
+        <section>
+            <table style=" border-collapse: collapse;">
+                <!-- <tr>
+                    <th style="border: 1px dotted black;border-collapse: collapse;padding: 5px;"></th>
+                    <th style="border: 1px dotted black;border-collapse: collapse;padding: 5px;"></th>
+                    <th style="border: 1px dotted black;border-collapse: collapse;padding: 5px;"></th>
+                    <th style="border: 1px dotted black;border-collapse: collapse;padding: 5px;"></th>
+
+                </tr> -->
+                <?php foreach ($result as $row) : ?>
+                    <tr>
+                    <!-- style="border: 1px dotted black;border-collapse: collapse;padding: 5px;" -->
+                    <!-- <div class="otherBox"> -->
+                        <th style="background-color: #EDEDED;
+    border: 2px solid black;
+    padding: 50px;
+    margin: 20px;
+    border-radius: 30px;
+    /* margin-left: 520px; */
+    padding-left: 20px;
+    padding-top: 20px;
+    position: relative;
+    width: 300px;
+    padding-right: 20px;
+    height: 250px;
+    padding-bottom: 20px;">Name: <?php echo $row['username']; ?></th>
+                    <!-- </div> -->
+                        <!-- <td style="border: 1px dotted black;border-collapse: collapse;padding: 5px;"></td> -->
+                    </tr>
+                <?php endforeach; ?>
+                
+                
+            </table>
+        </section>
+        
 
         <?php foreach ($query as $name) : 
             echo 'after for each'; ?>
