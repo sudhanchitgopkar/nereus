c94e14b670efa5b0c5850f2987e60dc7a0dbcdf8
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-12-01 10:58:25 -0500

    weirdness with browse

--- browse.php
+++ browse.php
@@ -10,7 +10,17 @@
     </head>
     <body>
         <div style="text-align:center;">
-            <span class="N-logo">N.</span>
+            <div>
+                <button class="dropbtn" onclick="myFunction()">Dropdown
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content" id="myDropdown">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#">Link 3</a>
+                </div>
+                <span class="N-logo">N.</span>
+            </div>
             <br>
             <br>
             <br>
@@ -98,7 +108,23 @@
                 </div>
             </div>
         </div>
-        
+        <script>
+            /* When the user clicks on the button, 
+            toggle between hiding and showing the dropdown content */
+            function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+            }
+
+            // Close the dropdown if the user clicks outside of it
+            window.onclick = function(e) {
+            if (!e.target.matches('.dropbtn')) {
+            var myDropdown = document.getElementById("myDropdown");
+                if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+                }
+            }
+            }
+        </script>
     </body>
 
     
