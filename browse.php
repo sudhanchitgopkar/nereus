0d23b5070834da7a0471800e291eb7ef97f487f8
sabamerchant <57716625+sabamerchant@users.noreply.github.com>
2021-12-01 20:55:58 -0500

    menu hamburger thing kinda working

--- browse.php
+++ browse.php
@@ -35,12 +35,17 @@
             <div>
                 <div class="N-logo">N.</div>
 
-                <div class="dropdown" style="padding-top: 39px !important;">
-                    <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                <div class="dropdown" style="padding-top: 38px !important;">
+                    <button onclick="myFunction()" class="dropbtn">
+                        <!-- Dropdown -->
+                        <div class="hamburger"></div>
+                        <div class="hamburger"></div>
+                        <div class="hamburger"></div>
+                    </button>
                     <div id="myDropdown" class="dropdown-content">
-                        <a href="#home">Home</a>
-                        <a href="#about">About</a>
-                        <a href="#contact">Contact</a>
+                        <a href="index.html">Home</a>
+                        <a href="browse.php">Browse</a>
+                        <a href="#contact">Log Out</a>
                     </div>
                 </div>
             </div>
