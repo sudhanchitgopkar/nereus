df4342e8d1342b6ead01296abbaa427a019d938e
Venusly Yang <venuslyyang@s172-20-116-h77.paws.uga.edu>
2021-12-05 20:13:20 -0500

    connected php to login anf register new design

--- LogIn.php
+++ LogIn.php
@@ -43,8 +43,96 @@
      }
 
 ?>
+<!doctype html>
+<html lang="en">
 
-<DOCTYPE! html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Nunito' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+
+    <title>Hello, world!</title>
+</head>
+
+<body><script type="text/javascript">
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  function myFunction() {
+      document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+              var openDropdown = dropdowns[i];
+              if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+              }
+          }
+      }
+  }
+</script>
+<div style="text-align:center;">
+  <div>
+      <div class="N-logo"><a href="#">N.</a></div>
+      <div class="dropdown" style="padding-top: 38px !important; margin-right: 30px;">
+          <button onclick="myFunction()" class="dropbtn">
+              <!-- Dropdown -->
+              <div class="hamburger"></div>
+              <div class="hamburger"></div>
+              <div class="hamburger"></div>
+          </button>
+          <div id="myDropdown" class="dropdown-content">
+              <a href="#">Home</a>
+              <a href="#">Browse</a>
+              <a href="#">Log Out</a>
+          </div>
+      </div>
+  </div>
+    
+    <div class="mainbody d-flex">
+        <div class="container d-flex justify-content-center my-auto">
+            <div class="row g-0" id="logrow">
+                <div class="col-lg-6 mx-0 my-0">
+                    <img class="img-fluid" src="diversity.jpeg" id="loginimg">
+                </div>
+                <div class="col-lg-6 pt-5 px-5">
+                    <h1>Log In</h1>
+                    <p>Welcome back!
+                    <p>
+                    <form method="post">
+                        <div class="form-row mb-3 mt-4">
+                            <input type="text" placeholder="Username" name="username" required>
+                        </div>
+                        <div class="form-row mb-4">
+                            <input type="password" placeholder="Password" name="password"required>
+                        </div>
+                        <div class="form-row mb-2">
+                            <button class="accbtn mb-2" type="submit">Submit</button>
+                            <button class="regbtn">Forgot Password</button>
+                        </div>
+                        <div class="form-row">
+                            <p class="mb-0"> New to Nereus? Register <a href="registerNew.html">here</a>.</p>
+                            <p> or learn more about us <a href="">here</a>.</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+       
+        </div>
+    <!---</div>--->
+</body>
+</html>
+
+<!-- <DOCTYPE! html>
   <html>
     <head>
       <link rel="stylesheet" href="logstyle.css" />
@@ -77,4 +165,4 @@
 
     </body>
   </html>
-</DOCTYPE>
+</DOCTYPE> -->
--- register.php
+++ register.php
@@ -47,9 +47,9 @@
               $username = $_POST['username'];
               $_SESSION['username'] = $username;
               $url = "cp.php";
-              // header('Location: browse.php');
+              header('Location: browse.php');
   
-              header(string: 'Location: ' . "cp.php");
+              //header(string: 'Location: ' . "cp.php");
               exit();
             }
             // if(!isset($_SESSION['username'])) {
@@ -71,7 +71,94 @@
         }
      
 ?>
+<!doctype html>
+<html lang="en">
 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <script type="text/javascript">
+        /* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+    </script>
+    <div style="text-align:center;">
+        <div>
+            <div class="N-logo"><a href="#">N.</a></div>
+            <div class="dropdown" style="padding-top: 38px !important; margin-right: 30px;">
+                <button onclick="myFunction()" class="dropbtn">
+                    <!-- Dropdown -->
+                    <div class="hamburger"></div>
+                    <div class="hamburger"></div>
+                    <div class="hamburger"></div>
+                </button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Home</a>
+                    <a href="#">Browse</a>
+                    <a href="#">Log Out</a>
+                </div>
+            </div>
+        </div>
+        
+    <div class="mainbody d-flex">
+        <div class="container d-flex justify-content-center my-auto">
+            <div class="row g-0">
+                <div class="col-lg-6 mx-0 my-0">
+                    <img class="img-fluid" src="diversity.jpeg" id="loginimg">
+                </div>
+                <div class="col-lg-6 pt-5 px-5">
+                    <h1>Register</h1>
+                    <p>Let's Get Started!<p>
+                    <form method="post">
+                        <div class="form-row mb-2 mt-4">
+                            <input type="text" placeholder="Username" name ="username" required>
+                        </div>
+                        <div class="form-row mb-2">
+                            <input type="email" placeholder="Email" name ="email" required>
+                        </div>
+                        <div class="form-row mb-2">
+                            <input type="password" placeholder="Password" name ="password" required>
+                        </div>
+                        <div class="form-row mb-2">
+                            <button class="accbtn mb-2" type="submit">Submit</button>
+                            <button class="regbtn">Learn More</button>
+                        </div>
+                        <div class="form-row">
+                            <p class="mb-0"> Have an account? Login <a href="LogIn.php">here</a>.</p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<!-- </html>
 <DOCTYPE! html>
   <html>
     <head>
@@ -113,4 +200,4 @@
 
     </div>
   </html>
-</DOCTYPE>
+</DOCTYPE> -->
